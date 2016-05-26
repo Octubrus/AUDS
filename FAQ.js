3f4c7abb873de1c068c956de936659516bf1eccf
Augustinas Venclovas <augustinasv@gmail.com>
2016-05-27 00:29:34 +0100

    worked on google map

--- FAQ.js
+++ FAQ.js
@@ -7,7 +7,8 @@ function initMap() {
 
   var map = new google.maps.Map(document.getElementById('mapAberdeen'), {
     zoom: 15,
-    center: {lat: 57.166130, lng: -2.098298},  // Brooklyn.
+    center: {lat: 57.166130, lng: -2.098298},  // MacRobert.
+    disableDefaultUI: true,
     mapTypeControlOptions: {
       mapTypeIds: [google.maps.MapTypeId.ROADMAP, customMapTypeId]
     }
@@ -15,20 +16,24 @@ function initMap() {
 
   map.mapTypes.set(customMapTypeId, customMapType);
   map.setMapTypeId(customMapTypeId);
-}
 
-
-
-/*
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('mapAberdeen'), {
-  center: {lat: 57.166130, lng: -2.098298},
-  zoom: 15
+  var MacRobertMarker = new google.maps.Marker({
+    position: {lat: 57.166130, lng: -2.098298},
+    animation: google.maps.Animation.DROP,
+    title:"Hello World!"
   });
-  var stylesArray = [{"featureType":"all","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"administrative.country","elementType":"labels.text.fill","stylers":[{"color":"#ed5929"}]},{"featureType":"administrative.locality","elementType":"labels.text.fill","stylers":[{"color":"#c4c4c4"}]},{"featureType":"administrative.neighborhood","elementType":"labels.text.fill","stylers":[{"color":"#ffcb05"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21},{"visibility":"on"}]},{"featureType":"poi.business","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffcb05"},{"lightness":"0"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"labels.text.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.highway","elementType":"labels.text.stroke","stylers":[{"color":"#ed5929"},{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffcb05"}]},{"featureType":"road.arterial","elementType":"labels.text.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.arterial","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"},{"visibility":"off"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"#666666"}]},{"featureType":"road.local","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]
+  var JohnstoneHallMarker = new google.maps.Marker({
+    position: {lat: 57.162711, lng: -2.103072},
+    animation: google.maps.Animation.DROP,
+    title:"Hello World!"
+  });
 
+  // To add the marker to the map, call setMap();
+  MacRobertMarker.setMap(map);
+  JohnstoneHallMarker.setMap(map);
 }
+
+
 
 
 /*
