c7e26c42b698c795136f4a04346df16116e049f5
Augustinas Venclovas <augustinasv@gmail.com>
2016-05-26 14:48:52 +0100

    snazzy map added as FAQ bg

--- FAQ.js
+++ FAQ.js
@@ -1,29 +1,20 @@
-function initialize() {
+function initMap() {
+  var customMapType = new google.maps.StyledMapType([{"featureType":"all","elementType":"all","stylers":[{"saturation":"0"}]},{"featureType":"all","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"all","elementType":"geometry.fill","stylers":[{"visibility":"simplified"}]},{"featureType":"all","elementType":"geometry.stroke","stylers":[{"visibility":"simplified"}]},{"featureType":"all","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"simplified"},{"color":"#efc337"},{"lightness":"-15"},{"gamma":"1.00"}]},{"featureType":"administrative","elementType":"labels.icon","stylers":[{"saturation":"-100"},{"lightness":"20"},{"visibility":"simplified"},{"gamma":".5"}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"lightness":"60"},{"saturation":"0"},{"color":"#efc337"},{"visibility":"simplified"}]},{"featureType":"landscape.natural","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#efc337"},{"saturation":"0"},{"lightness":"12"},{"gamma":"1.00"}]},{"featureType":"landscape.natural","elementType":"labels.text","stylers":[{"lightness":"100"}]},{"featureType":"landscape.natural","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural.landcover","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural.terrain","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"color":"#efc337"},{"saturation":"0"},{"lightness":"-7"},{"gamma":"1.00"}]},{"featureType":"road","elementType":"all","stylers":[{"gamma":"6.14"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#efc337"},{"saturation":"0"},{"lightness":"-18"},{"gamma":"1.00"}]},{"featureType":"road","elementType":"labels.text","stylers":[{"visibility":"simplified"},{"saturation":"0"},{"lightness":"100"}]},{"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"},{"invert_lightness":true}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"simplified"},{"hue":"#ffc200"},{"saturation":"100"},{"gamma":"0.85"},{"lightness":"17"},{"weight":"1.00"}]},{"featureType":"transit","elementType":"labels.icon","stylers":[{"hue":"#ffc200"},{"saturation":"-100"},{"gamma":"0.5"},{"weight":"1.00"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"water","elementType":"geometry","stylers":[{"lightness":"80"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#efc337"},{"saturation":"0"},{"lightness":"35"},{"gamma":"1.00"}]},{"featureType":"water","elementType":"labels.text","stylers":[{"visibility":"off"}]}]
+   ,{
+      name: 'Custom Style'
+  });
+  var customMapTypeId = 'custom_style';
 
-  // Create an array of styles.
-  var stylesArray = [{"featureType":"all","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"administrative.country","elementType":"labels.text.fill","stylers":[{"color":"#ed5929"}]},{"featureType":"administrative.locality","elementType":"labels.text.fill","stylers":[{"color":"#c4c4c4"}]},{"featureType":"administrative.neighborhood","elementType":"labels.text.fill","stylers":[{"color":"#ffcb05"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21},{"visibility":"on"}]},{"featureType":"poi.business","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffcb05"},{"lightness":"0"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"labels.text.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.highway","elementType":"labels.text.stroke","stylers":[{"color":"#ed5929"},{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffcb05"}]},{"featureType":"road.arterial","elementType":"labels.text.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.arterial","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"},{"visibility":"off"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"#666666"}]},{"featureType":"road.local","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]
-
-
-  // Create a new StyledMapType object, passing it the array of styles,
-  // as well as the name to be displayed on the map type control.
-  var styledMap = new google.maps.StyledMapType(stylesArray,
-    {name: "Styled Map"});
-
-  // Create a map object, and include the MapTypeId to add
-  // to the map type control.
-  var mapOptions = {
+  var map = new google.maps.Map(document.getElementById('mapAberdeen'), {
     zoom: 15,
-    center: new google.maps.LatLng(57.166130, -2.098298),
+    center: {lat: 57.166130, lng: -2.098298},  // Brooklyn.
     mapTypeControlOptions: {
-      mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+      mapTypeIds: [google.maps.MapTypeId.ROADMAP, customMapTypeId]
     }
-  };
-  var map = new google.maps.Map(document.getElementById('mapAberdeen'),
-    mapOptions);
+  });
 
-  //Associate the styled map with the MapTypeId and set it to display.
-  map.mapTypes.set('map_style', styledMap);
-  map.setMapTypeId('map_style');
+  map.mapTypes.set(customMapTypeId, customMapType);
+  map.setMapTypeId(customMapTypeId);
 }
 
 
