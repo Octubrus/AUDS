ce41d63f67e8d3595c753ef691c49ad2433713ba
Augustinas Venclovas <augustinasv@gmail.com>
2016-06-18 00:38:58 +0100

    worked on classes.js

--- Classes.js
+++ Classes.js
@@ -1,11 +1,32 @@
-$(function() {
+$( document ).ready(function() {
+  var classButtons = $('li.classButton');
+  var button1 = $('li#Button1');
+  var button2 = $('li#Button2');
 
-  var classButton = $('li.classButton');
+  var classContainer = $('div.classContainer');
+  var picture1 = $('div#1Class');
+  var picture2 = $('div#2Class');
 
 
-  classButton.on('click', function() {
-    classButton.removeClass('pink');
-    $(this).addClass('pink');
+  $('ul#classList').on('click', 'li', function() {
+    $('li.classButton').removeClass('pink');
+    var $this = $(this);
+    $this.addClass('pink');
+
+    $('div.classContainer').css("marginLeft", "1000px");
+    var classNumber = $this.index();
+    $('div.classContainer:eq(classNumber)').css("marginLeft", "100px");
+
+
+    /*classContainer.css("opacity", "0.0");
+    picture1.css("opacity", "1.0");*/
+  });
+  button2.on('click', function() {
+    classButtons.removeClass('pink');
+    button2.addClass('pink');
+
+  /*  classContainer.css("opacity", "0.0");
+    picture2.css("opacity", "1.0");*/
   });
 
 });
