570ac0431b04ba59f61790a31c995969c7520546
Augustinas Venclovas <augustinasv@gmail.com>
2016-06-18 22:57:09 +0100

    classes.js done for desktop, styling started

--- Classes.js
+++ Classes.js
@@ -1,32 +1,17 @@
 $( document ).ready(function() {
   var classButtons = $('li.classButton');
-  var button1 = $('li#Button1');
-  var button2 = $('li#Button2');
-
   var classContainer = $('div.classContainer');
-  var picture1 = $('div#1Class');
-  var picture2 = $('div#2Class');
+  var classList = $('ul#classList');
 
-
-  $('ul#classList').on('click', 'li', function() {
-    $('li.classButton').removeClass('pink');
+  classList.on('click', 'li', function() {
+    classButtons.removeClass('clicked');
     var $this = $(this);
-    $this.addClass('pink');
+    $this.addClass('clicked');
 
-    $('div.classContainer').css("marginLeft", "1000px");
+    classContainer.hide();
     var classNumber = $this.index();
-    $('div.classContainer:eq(classNumber)').css("marginLeft", "100px");
+    $('div.classContainer:eq('+classNumber+')').show();
 
-
-    /*classContainer.css("opacity", "0.0");
-    picture1.css("opacity", "1.0");*/
-  });
-  button2.on('click', function() {
-    classButtons.removeClass('pink');
-    button2.addClass('pink');
-
-  /*  classContainer.css("opacity", "0.0");
-    picture2.css("opacity", "1.0");*/
   });
 
 });
