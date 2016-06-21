1cb3f552e374322cd72c48d541c8a90f88d51430
Augustinas Venclovas <augustinasv@gmail.com>
2016-06-21 22:31:40 +0100

    classes page optimized for mobile

--- Classes.js
+++ Classes.js
@@ -2,6 +2,12 @@ $( document ).ready(function() {
   var classButtons = $('li.classButton');
   var classContainer = $('div.classContainer');
   var classList = $('ul#classList');
+  var sideMenu = $('div.sideMenu');
+  var sideContent = $('div.sideContent');
+  var overlay = $('div#overlay');
+  var exitButton = $('div.exitButton');
+
+  exitButton.hide();
 
   classContainer.hide();
 
@@ -21,7 +27,43 @@ $( document ).ready(function() {
     classContainer.hide();
     var classNumber = $this.index();
     $('div.classContainer:eq('+classNumber+')').show();
-
   });
+
+  var isMobile = window.matchMedia("only screen and (max-width: 760px)");
+
+  if (isMobile.matches) {
+    classList.on('click', 'li', function() {
+      classButtons.removeClass('clicked');
+      var $this = $(this);
+      $this.addClass('clicked');
+
+      sideMenu.removeClass('col-xs-12').addClass('hidden-xs');
+      sideContent.removeClass('hidden-xs').addClass('col-xs-12');
+
+      classContainer.hide();
+      var classNumber = $this.index();
+      var clickedClass = $('div.classContainer:eq('+classNumber+')');
+
+      clickedClass.show().addClass('backgroundPositionMobile');
+
+      clickedClass.children().addClass('classContentMobile');
+      overlay.addClass('backgroundOpacity');
+      exitButton.show();
+    });
+
+    exitButton.on('click', function() {
+
+      sideMenu.removeClass('hidden-xs').addClass('col-xs-12');
+      sideContent.removeClass('col-xs-12').addClass('hidden-xs')
+
+      classContainer.hide().removeClass('backgroundPositionMobile');
+
+      classContainer.children().removeClass('classContentMobile');
+      overlay.removeClass('backgroundOpacity');
+      exitButton.hide();
+
+    });
+
+  }
 
 });
